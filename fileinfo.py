55d8ce110d7a63b9b5b20dceb55468ac30694d8f
Shuai Zhang <2892928163@qq.com>
2020-03-21 11:19:42 +0800

    initial update

---
+++ fileinfo.py
@@ -0,0 +1,8 @@
+# Scrift Name        : fileinfo.py
+# Author                 : Not sure where Igot this from
+# Created                : 28th November 2011
+# Last Modified      :
+# Version                : 1.0
+# Modification       :
+
+# Description            : Show file information for a given file
